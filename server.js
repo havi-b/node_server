61cc9e9a2789ba773d0c626b4c0eebb24f57e52d
Hafzul Bhuiyan <hbhuiyan@e1z3r4p21.42.us.org>
2019-07-18 22:46:17 -0700

    Initial Commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// Bodyparser Middleware
+app.use(bodyParser.json());
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect MongoDB
+mongoose
+    .connect(db, {useNewUrlParser: true})
+    .then(() => console.log('MongoBD Connected...'))
+    .catch(err => console.log(err))
+
+// Use Environment variable for PORT or 5000
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
